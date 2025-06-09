74eab2d7744fa131ebb0f1384831d5024d916828
James Hightower <9148229+neuvo@users.noreply.github.com>
2025-06-09 12:08:42 -0700

    more changes missed in initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# complyUI: An interface for resolving compliance violations
+
+## How to run locally
+From root directory:
+
+`$ npm install`
+
+`$ npm start`
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Paragraph from './Paragraph.js';
+import Violations from './Violations.js';
 
 export default function ComplianceDashboard() {
     const [paragraphText, setParagraphText] = useState('Hello world!');
     return <>
         <div id='paragraph-div'><Paragraph id='paragraph' text={paragraphText} setText={setParagraphText}/></div>
-        <div id='violations-div'></div>
+        <div id='violations-div'><Violations /></div>
     </>
 }
